14d456bd3969e5e6acf43004bc934a881d2d9a87
Lucasark <Lucas.arkantos@hotmail.com>
2018-06-28 21:08:09 -0300

    Ygor safado

--- src/Principal.java
+++ src/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 	
     public static void main(String[] args) {
     	List<Cliente> clientes = new ArrayList<Cliente>(0);
-    	Carrinho carrinho;
+    	List<Carrinho> carrinhos = new ArrayList<Carrinho>(0);
     	int h = 0;
     	int total = 0;
     	Menu menu = new Menu();
@@ -29,6 +29,8 @@ public class Principal {
 							Cliente cliente = new Cliente();
 							cliente = cliente.leCliente();
 							clientes.add(cliente);
+							Carrinho carrinho = new Carrinho();
+							carrinhos.add(carrinho);
 							System.out.println("----Cliente criado----");
 						}
 					}
@@ -37,12 +39,26 @@ public class Principal {
 						int q = 1;
 						for(Cliente m: clientes) {
 				    		System.out.println("[" + q + "]"+m.getnome());
+				    		q++;
 				    	}
-						System.out.println(clientes.get(q-1).getSenha());
+						//System.out.println(clientes.get(q-1).getSenha());
 						System.out.print("Digite sua senha:");
-						int w = input.nextInt();
-						if (w == clientes.get(q-1).getSenha()) {
-							menu.leNomesEventos();
+						int senha = input.nextInt();
+						if (senha == clientes.get(q-1).getSenha()) {
+							//----A.FAMIGERADA.COMPRA.COM
+							System.out.println("Selecionar Evento: ");
+							int y = 0, F =0;
+							List<String> temp = new ArrayList<String>(0);
+							do {
+								menu.leNomesEventos();
+								w = input.nextInt();
+								temp.add(e);
+								System.out.println("Deseja Continuar? [0] Sim [1] Não");
+								y = input.nextInt();
+								F++;
+							} while (y != 1);
+							carrinhos.get(q-1);
+							
 						}
 					}
 					break;
